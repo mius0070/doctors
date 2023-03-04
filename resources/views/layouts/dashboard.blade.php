19b8740e9d365290c47582e1d9f1f8d9f5c3d3bb
Mourad <chibanemourad1@gmail.com>
2023-03-04 14:41:45 +0100

    fix spinner

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -22,9 +22,9 @@
     <!-- Site wrapper -->
     <div class="wrapper">
         <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60"
-                width="60">
+        <div class="preloader flex-column justify-content-center align-items-center" style="opacity: 0.5">
+            <img class="" src="{{ asset('dist/img/spinner.gif') }}" alt="spinner" height="100"
+                width="100">
         </div>
 
         {{-- navbar --}}
@@ -34,8 +34,8 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="../../index3.html" class="brand-link">
-                <img src="{{ asset('/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+            <a href="/" class="brand-link">
+                <img src="{{ asset('/dist/img/logo.png') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Doctors</span>
             </a>
